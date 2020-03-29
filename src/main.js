fd303bb84363744f1634c666a0d9700d8b81de44
Mário <mariosergio952@hotmail.com.br>
2020-03-29 18:51:23 -0300

    Implementação da lógica

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 let isStart = false;
+let selectFirstAnimal = null;
 
 function startGame() {
   isStart = true;
@@ -7,4 +8,31 @@ function startGame() {
 
 function finishGame() {
   $('#btn-finish-game').attr('disabled', 'true');
+}
+
+function selectItem(animal) {
+  if (isStart) {
+    showItem(animal);
+    if (selectFirstAnimal === null) {
+      selectFirstAnimal = animal;
+    } else {
+      if (sameItens(animal, selectFirstAnimal)) {
+        hideIten(animal);
+        selectFirstAnimal = null
+        alert('item será removido (2)');
+      }
+    }
+  }
+}
+
+function sameItens(animal, selectFirstAnimal) {
+  return animal === selectFirstAnimal;
+}
+
+function hideIten(animal) {
+
+}
+
+function showItem() {
+
 }
