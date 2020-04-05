3dcca6d132df1e7cfbadccfcf35ae4e1af225f0c
Mário <mariosergio952@hotmail.com.br>
2020-04-05 18:17:45 -0300

    implementação jogo da memoria

--- src/main.js
+++ src/main.js
@@ -1,245 +1,172 @@
-let animais = [
-    {
-        id: 1,
-        nome: 'Leão'
-    },
-    {
-        id: 2,
-        nome: 'Macaco'
-    },
-    {
-        id: 3,
-        nome: 'Porco'
-    },
-    {
-        id: 4,
-        nome: 'Cachorro'
-    },
-    {
-        id: 5,
-        nome: 'Aranha'
-    },
-    {
-        id: 6,
-        nome: 'Avestruz'
-    },
-    {
-        id: 7,
-        nome: 'Andorinha'
-    },
-    {
-        id: 8,
-        nome: 'Baleia'
-    },
-    {
-        id: 9,
-        nome: 'Besouro'
-    },
-    {
-        id: 10,
-        nome: 'Bezerro'
-    },
-    {
-        id: 11,
-        nome: 'Boi'
-    },
-    {
-        id: 12,
-        nome: 'Borboleta'
-    },
-    {
-        id: 13,
-        nome: 'Cabra'
-    },
-    {
-        id: 14,
-        nome: 'Camaleão'
-    },
-    {
-        id: 15,
-        nome: 'Camelo'
-    },
-    {
-        id: 16,
-        nome: 'Canguru'
-    },
-    {
-        id: 17,
-        nome: 'Caranguejo'
-    },
-    {
-        id: 18,
-        nome: 'Cavalo'
-    },
-    {
-        id: 19,
-        nome: 'Cegonha'
-    },
-    {
-        id: 20,
-        nome: 'Cobra'
-    },
-    {
-        id: 21,
-        nome: 'Coelho'
-    },
-    {
-        id: 22,
-        nome: 'Dinossauro'
-    },
-    {
-        id: 23,
-        nome: 'Elefante'
-    },
-    {
-        id: 24,
-        nome: 'Ema'
-    },
-    {
-        id: 25,
-        nome: 'Escorpião'
-    },
-    {
-        id: 26,
-        nome: 'Esponja'
-    },
-    {
-        id: 27,
-        nome: 'Falcão'
-    },
-    {
-        id: 28,
-        nome: 'Flamingo'
-    },
-    {
-        id: 29,
-        nome: 'Foca'
-    },
-    {
-        id: 30,
-        nome: 'Formiga'
-    },
-    {
-        id: 31,
-        nome: 'Gafanhoto'
-    },
-    {
-        id: 32,
-        nome: 'Gaivota'
-    },
-    {
-        id: 33,
-        nome: 'Galinha'
-    },
-    {
-        id: 34,
-        nome: 'Gambá'
-    },
-    {
-        id: 35,
-        nome: 'Gato'
-    },
-    {
-        id: 36,
-        nome: 'Jacaré'
-    },
-    {
-        id: 37,
-        nome: 'Lagarto'
-    },
-    {
-        id: 38,
-        nome: 'Lobo'
-    },
-    {
-        id: 39,
-        nome: 'Onça'
-    },
-    {
-        id: 40,
-        nome: 'Papagaio'
-    },
-    {
-        id: 41,
-        nome: 'Panda'
-    },
-    {
-        id: 42,
-        nome: 'Peixe'
-    },
-    {
-        id: 43,
-        nome: 'Peru'
-    },
-    {
-        id: 44,
-        nome: 'Porco'
-    },
-    {
-        id: 45,
-        nome: 'Raposa'
-    },
-    {
-        id: 46,
-        nome: 'Sapo'
-    },
-    {
-        id: 47,
-        nome: 'Tatu'
-    },
-    {
-        id: 48,
-        nome: 'Tubarão'
-    },
-    {
-        id: 49,
-        nome: 'Urso'
-    },
-    {
-        id: 50,
-        nome: 'Vaca'
-    },
-    {
-        id: 51,
-        nome: 'Zebra'
-    }
-]
-
 let isStart = false;
+let hourStart = null;
+let hourEnd = null;
 let selectFirstAnimal = null;
+let listaGeral = [];
+let level = 12;
+
+let templateItem = `
+<div class="card col-md-2 pointer" onclick="selectItem('{name}', {rowId})" id="{name}-{rowId}">
+  <div class="card-body">
+    <p class="card-text d-none">{name}</p>
+  </div>
+</div>`;
+
+startGame();
 
 function startGame() {
-  isStart = true;
-  $('#btn-finish-game').removeAttr('disabled');
+  listaGeral = [];
+  if (!isStart) {
+    hourStart = Date.now();
+    isStart = true;
+    duplicateAllItens(level);
+    $('#btn-start-game').attr('disabled', 'true');
+    $('#btn-finish-game').removeAttr('disabled');
+  }
 }
 
 function finishGame() {
+  listaGeral = [];
+  isStart = false;
+  $('#btn-start-game').removeAttr('disabled');
   $('#btn-finish-game').attr('disabled', 'true');
 }
 
-function selectItem(animal) {
+function duplicateAllItens(level) {
+  let numbersRandom = [];
+  let animaisRandom = [];
+  let column = 4;
+  let rows = 6;
+
+  for (let i = 0; i < level; i++) {
+    numbersRandom.push(getRandomArbitrary(1, animais.length));
+  }
+
+  animaisRandom = animais.filter((item, count) => {
+    return numbersRandom.includes(item.id);
+  });
+
+  animaisRandom.forEach((item) => {
+    item.disable = false;
+    listaGeral.push(item);
+    listaGeral.push(item);
+  });
+
+  listaGeral = embaralhar(listaGeral);
+
+  let textHtml = '';
+  listaGeral.forEach((item, rowId) => {
+    let itemHtml = templateItem;
+
+    itemHtml = itemHtml.replace(/{name}/g, item.nome);
+    itemHtml = itemHtml.replace(/{rowId}/g, (rowId + 1));
+
+    textHtml += itemHtml;
+  });
+
+  $('.game-list').html(textHtml);
+}
+
+/**
+ * https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ * @param {*} min 
+ * @param {*} max 
+ */
+function getRandomArbitrary(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+/**
+ * http://filipeteixeira.com.br/blog/2015/11/19/embaralhar-uma-array-em-javascript/
+ * @param {*} array 
+ */
+function embaralhar(array) {
+  let indice_atual = array.length, valor_temporario, indice_aleatorio;
+
+  while (0 !== indice_atual) {
+
+    indice_aleatorio = Math.floor(Math.random() * indice_atual);
+    indice_atual -= 1;
+
+    valor_temporario = array[indice_atual];
+    array[indice_atual] = array[indice_aleatorio];
+    array[indice_aleatorio] = valor_temporario;
+  }
+
+  return array;
+}
+
+/**
+ * 
+ * @param {*} animal 
+ * @param {*} id 
+ */
+function selectItem(animal, id) {
+  let selectedAnimal = { name: animal, id };
   if (isStart) {
-    showItem(animal);
+
+    if (isDisabled(selectedAnimal)) {
+      return false;
+    }
+
+    showItem(selectedAnimal);
     if (selectFirstAnimal === null) {
-      selectFirstAnimal = animal;
+      selectFirstAnimal = selectedAnimal;
     } else {
-      if (sameItens(animal, selectFirstAnimal)) {
-        hideIten(animal);
-        selectFirstAnimal = null
-        alert('item será removido (2)');
+      if (sameItens(selectedAnimal, selectFirstAnimal)) {
+        showItem(selectFirstAnimal);
+        disableItem(selectedAnimal);
+        if (isFinish()) {
+          hourEnd = Date.now();
+          alert('Ganhou');
+        }
+        selectFirstAnimal = null;
+      } else {
+        setTimeout(() => {
+          hideIten(selectedAnimal);
+          hideIten(selectFirstAnimal);
+          selectFirstAnimal = null;
+        }, 1000);
       }
     }
   }
 }
 
-function sameItens(animal, selectFirstAnimal) {
-  return animal === selectFirstAnimal;
+function disableItem(selectedAnimal) {
+  let itens = listaGeral.filter((item) => {
+    return item.nome === selectedAnimal.name;
+  });
+
+  itens.forEach((item) => {
+    item.disable = true;
+  });
+}
+
+function isDisabled(selectedAnimal) {
+  return (listaGeral.filter((item) => {
+    return item.nome === selectedAnimal.name && item.disable == true;
+  }).length > 0);
+}
+
+function isFinish() {
+  return (listaGeral.filter((item) => {
+    return item.disable == false;
+  }).length === 0);
+}
+
+function sameItens(selectedAnimal, selectFirstAnimal) {
+  if (selectedAnimal.name === selectFirstAnimal.name &&
+    selectedAnimal.id !== selectFirstAnimal.id) {
+    return true;
+  }
 }
 
 function hideIten(animal) {
-
+  $(`#${animal.name}-${animal.id}`).children().children().addClass('d-none');
 }
 
-function showItem() {
-
+function showItem(animal) {
+  $(`#${animal.name}-${animal.id}`).children().children().removeClass('d-none');
 }
